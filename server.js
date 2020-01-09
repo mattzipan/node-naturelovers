169e4ab59ed0e521a314474437332565460180ba
mattzipan <mattzipan@gmail.com>
2020-01-09 08:40:45 +0800

    removed cl log

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const dotenv = require("dotenv");
 // setting environment variables
 dotenv.config({ path: "./config.env" });
-console.log(`Environment: ${process.env.NODE_ENV}`);
+// console.log(`Environment: ${process.env.NODE_ENV}`);
 
 //this should run after the environment setting from above
 const app = require("./app");
 
 //start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on ${port}...`));
+app.listen(port, () => {
+  console.log(`listening on ${port}...`);
+});
