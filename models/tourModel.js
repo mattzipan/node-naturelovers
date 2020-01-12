70d8ebb152921d1aa99ebeb60340744337668e5f
mattzipan <mattzipan@gmail.com>
2020-01-12 08:05:56 +0800

    updated Schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,24 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: "A tour must have a name!",
-    unique: true
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: "A tour must have a duration!"
+  },
+  maxGroupSize: {
+    type: Number,
+    required: "A tour must have a group size!"
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   rating: {
     type: Number,
@@ -15,7 +32,27 @@ const tourSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: "A tour must have a price!"
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: "A tour must have a summary!"
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: "A tour must have an image!"
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 // use Schema to make the Model
