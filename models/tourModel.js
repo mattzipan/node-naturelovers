f993f52f10c08f6d9923b07572ca9d89b8a22e9f
mattzipan <mattzipan@gmail.com>
2020-01-14 09:03:12 +0800

    added virtual field "durationWeeks"

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,65 +2,77 @@ const mongoose = require("mongoose");
 
 // create Schema
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: "A tour must have a name!",
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: "A tour must have a name!",
+      unique: true,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: "A tour must have a duration!"
+    },
+    maxGroupSize: {
+      type: Number,
+      required: "A tour must have a group size!"
+    },
+    difficulty: {
+      type: String,
+      required: "A tour must have a difficulty",
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult"
+      }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    rating: {
+      type: Number,
+      default: 4.5
+    },
+    price: {
+      type: Number,
+      required: "A tour must have a price!"
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: "A tour must have a summary!"
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: "A tour must have an image!"
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   },
-  duration: {
-    type: Number,
-    required: "A tour must have a duration!"
-  },
-  maxGroupSize: {
-    type: Number,
-    required: "A tour must have a group size!"
-  },
-  difficulty: {
-    type: String,
-    required: "A tour must have a difficulty",
-    enum: {
-      values: ["easy", "medium", "difficult"],
-      message: "Difficulty is either: easy, medium, difficult"
-    }
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: "A tour must have a price!"
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: "A tour must have a summary!"
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: "A tour must have an image!"
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+//add virtual fields. This will be calculated on every get request
+// virtual properties are not in the api response by default, it has to be specifically defined
+tourSchema.virtual("durationWeeks").get(function() {
+  return this.duration / 7;
 });
 
 // use Schema to make the Model
