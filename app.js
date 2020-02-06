7f298ab9814e3c6336d05a4697a7019861ad64b9
mattzipan <mattzipan@gmail.com>
2020-02-06 08:34:23 +0800

    added global error handler

--- app.js
+++ app.js
@@ -34,7 +34,33 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/api/v1/tours", tourRouter);
-
 app.use("/api/v1/users", userRouter);
+
+// handling unknown routes
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl}`
+  // })
+
+  const err = new Error(`Cant find ${req.originalUrl}`)
+  err.statusCode = 404
+  err.status = "fail"
+
+  // next handles ANY argument as an error and passes it to the global error handler in app.js, jumping over other middlewares
+  next(err)
+})
+
+// express interprates every middleware with 4 arguments as an error handler
+app.use((err, req, res, next) => {
+
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
+})
 
 module.exports = app;
