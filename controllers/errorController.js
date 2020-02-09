bf0e2ddba21d52b8e726802a5a9904524867ada2
mattzipan <mattzipan@gmail.com>
2020-02-10 07:37:32 +0800

    added environment dependt errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,31 @@
+const sendErrorProd = (err, res) => {
+    // Operational trusted error: send msg to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    } else {
+        // log error
+        console.error("ERROR", err)
+
+        // send generic message
+        res.status(500).json({
+            status: "error",
+            message: "Oh oh, something went wrong :("
+        })
+    }
+}
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
 module.exports = (err, req, res, next) => {
     // log stackTrace
     console.log(err.stack)
@@ -5,8 +33,9 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || "error"
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res)
+    } else if (process.env.NODE_ENV === "production") {
+        sendErrorProd(err, res)
+    }
 }
