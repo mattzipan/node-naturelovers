628e62966027870935006639aafbafa1584066f3
mattzipan <mattzipan@gmail.com>
2020-02-09 12:05:18 +0800

    added error controller

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const appError = require("./utils/appError")
+const globalErrorHandler = require("./controllers/errorController")
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -43,24 +44,15 @@ app.all("*", (req, res, next) => {
   //   message: `Can't find ${req.originalUrl}`
   // })
 
-  const err = new Error(`Cant find ${req.originalUrl}`)
-  err.statusCode = 404
-  err.status = "fail"
+  // const err = new Error(`Cant find ${req.originalUrl}`)
+  // err.statusCode = 404
+  // err.status = "fail"
 
   // next handles ANY argument as an error and passes it to the global error handler in app.js, jumping over other middlewares
-  next(err)
+  next(new appError(`Cant find ${req.originalUrl}`, 404))
 })
 
 // express interprates every middleware with 4 arguments as an error handler
-app.use((err, req, res, next) => {
-
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || "error"
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler)
 
 module.exports = app;
