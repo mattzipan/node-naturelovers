ee398146fd7caf1b5aa823e9983a2559f5b368d9
mattzipan <mattzipan@gmail.com>
2020-02-10 08:29:36 +0800

    added errors for invalid IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("../utils/appError")
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
 const sendErrorProd = (err, res) => {
     // Operational trusted error: send msg to client
     if (err.isOperational) {
@@ -36,6 +43,9 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === "development") {
         sendErrorDev(err, res)
     } else if (process.env.NODE_ENV === "production") {
-        sendErrorProd(err, res)
+        let error = err
+        if (error.name === "CastError") error = handleCastErrorDB(error)
+
+        sendErrorProd(error, res)
     }
 }
