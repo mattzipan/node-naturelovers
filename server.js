d7aca2df0b9726a3d62c4dc2efd36c1fe5405816
mattzipan <mattzipan@gmail.com>
2020-02-12 05:01:49 +0800

    env logging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 // setting environment variables
 dotenv.config({ path: `${__dirname}/config.env` });
-// console.log(`Environment: ${process.env.NODE_ENV}`);
+console.log(`Environment: ${process.env.NODE_ENV}`);
 
 //configure mongoose
 // const DB = process.env.DATABASE.replace(
@@ -19,6 +19,7 @@ process.on("uncaughtException", err => {
 
 //this should run after the environment setting from above
 const app = require("./app");
+
 
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
