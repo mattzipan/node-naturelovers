1d1c70275c46a1ad868190f85d3eaef8ce89a2ff
mattzipan <mattzipan@gmail.com>
2020-02-12 05:01:28 +0800

    removed unused next arg

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel")
 const catchAsyncError = require("../utils/catchAsyncError")
 
-exports.signup = catchAsyncError(async (req, res, next) => {
+exports.signup = catchAsyncError(async (req, res) => {
     const newUser = await User.create(req.body)
 
     res.status(201).json({
