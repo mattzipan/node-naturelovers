72053c99cb3b3f95584e4d6e1e2817e98d882dec
mattzipan <mattzipan@gmail.com>
2020-02-13 07:01:11 +0800

    fixed vulnerability

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ const User = require("../models/userModel")
 const catchAsyncError = require("../utils/catchAsyncError")
 
 exports.signup = catchAsyncError(async (req, res) => {
-    const newUser = await User.create(req.body)
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    })
 
     res.status(201).json({
         status: "success",
