546e94ddf6145f4c27799f4d341274145cfca98e
mattzipan <mattzipan@gmail.com>
2020-02-13 09:54:46 +0800

    added get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
-exports.getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: "error", message: "this route is not defined" });
-};
+const User = require("../models/userModel")
+const catchAsyncError = require("../utils/catchAsyncError")
+
+exports.getAllUsers = catchAsyncError(async (req, res) => {
+
+  const users = await User.find()
+
+  res.status(200).json({
+    //following JSend formating standard
+    status: "success",
+    results: users.length,
+    data: {
+      users
+    }
+  });
+});
 
 exports.createUser = (req, res) => {
   res
